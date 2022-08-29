32066d560f1b6fe88176dccd169711a922fc6a9d
ptra <mdavis22@go.olemiss.edu>
2022-08-29 11:18:53 -0500

    a

--- qff_helper/qff_helper.py
+++ qff_helper/qff_helper.py
@@ -5,34 +5,55 @@ import subprocess
 
 
 class QffHelper:
-    def __init__(self):
-        pass
+    def __init__(self, _freqs_dir: str = "freqs"):
+        # TODO setup optional initializations with sane defaults
+        self.freqs_dir = _freqs_dir
+        self.freqs_files = {
+            "anpass": _freqs_dir + "/anpass.tmp",
+            "intder": _freqs_dir + "/intder.tmp",
+            "spectro": _freqs_dir + "/spectro.tmp",
+        }
+        self.input_files = {
+            "anpass1": _freqs_dir + "/anpass1.in",
+            "anpass2": _freqs_dir + "/anpass2.in",
+            "intder_geom": _freqs_dir + "/intder_geom.in",
+            "intder": _freqs_dir + "/intder.in",
+            "spectro": _freqs_dir + "/spectro.in",
+        }
+        self.output_files = {
+            "anpass1": _freqs_dir + "/anpass1.out",
+            "anpass2": _freqs_dir + "/anpass2.out",
+            "intder_geom": _freqs_dir + "/intder_geom.out",
+            "intder": _freqs_dir + "/intder.out",
+            "spectro": _freqs_dir + "/spectro.out",
+        }
 
     files = {}
+    freqs_files = {}
     energies = []
     relative_energies = []
-    freqs_dir = ""
+    freqs_dir = "freqs"
     anpass_refit_energy = 0.0
 
-    top_location = (
-        "/home/lailah/Documents/OleMiss/programming/Python/qff_helper/Programs"
-    )
+    #    top_location =   "/home/lailah/Documents/OleMiss/programming/Python/qff_helper/Programs"
     #    anpass_location = "/home/megan/Programs/anpass/anpass_cerebro.x"
     #    spectro_location = "/home/megan/Programs/spec3jm.ifort-00.static.x"
     #    intder_location = "/home/megan/Programs/intder/Intder2005.x"
-    anpass_location = top_location + "/anpass/anpass_cerebro.x"
-    spectro_location = top_location + "/spec3jm.ifort-00.static.x"
-    intder_location = top_location = "/intder/Intder2005.x"
+    #    anpass_location = top_location + "/anpass/anpass_cerebro.x"
+    #    spectro_location = top_location + "/spec3jm.ifort-00.static.x"
+    #   intder_location = top_location = "/intder/Intder2005.x"
+    programs = {
+        "anpass": "/home/megan/Programs/anpass/anpass_cerebro.x",
+        "spectro": "/home/megan/Programs/spec3jm.ifort-00.static.x",
+        "intder": "/home/megan/Programs/intder/Intder2005.x",
+    }
 
-    def setup_spectro_dir(
-        self, _files_dir: str, _intder_geom_file: str, _freqs_dir: str = "freqs"
-    ):
+    def setup_spectro_dir(self, _files_dir: str, _intder_geom_file: str):
         """Takes input directory with spectro.tmp, anpass.tmp and intder.tmp as well
         as location of intder.in for pts
         Makes a structured freqs directory with em
         """
         # TODO handle matdisp.pl stuff, but later
-        # TODO maybe move this to init function?
         try:
             with open(_intder_geom_file) as intder_file:
                 intder_lines = intder_file.readlines()
@@ -40,24 +61,24 @@ class QffHelper:
                 i for i, line in enumerate(intder_lines) if "DISP" in line
             ].pop()
             int_geom_header = intder_lines[0:disp_index]
+            #TODO might need to fix spacing here
             int_geom_header.append("DISP 1")
         except:
             print("Could not open inter_geom file")
 
-        if not os.path.exists(_freqs_dir):
-            os.makedirs(_freqs_dir)
-        with open(_freqs_dir + "/intder_geom.in", "w") as int_geom_file:
+        if not os.path.exists(self.freqs_dir):
+            os.makedirs(self.freqs_dir)
+        with open(self.freqs_dir + "/intder_geom.in", "w") as int_geom_file:
             for line in int_geom_header:
                 int_geom_file.write(line)
         self.files = {
-            "anpass_file": _files_dir + "/anpass.tmp",
-            "intder_file": _files_dir + "/intder.tmp",
-            "spectro_file": _files_dir + "/spectro.tmp",
+            "anpass": _files_dir + "/anpass.tmp",
+            "intder": _files_dir + "/intder.tmp",
+            "spectro": _files_dir + "/spectro.tmp",
         }
 
-        self.freqs_dir = _freqs_dir
         for file in self.files.values():
-            shutil.copy(file, _freqs_dir)
+            shutil.copy(file, self.freqs_dir)
 
     def make_relative_energies(self, _energy_file: str):
         """Converts energy.dat file to relative energies"""
@@ -76,18 +97,19 @@ class QffHelper:
     def run_anpass(self):
         """Passes relative energies into anpass"""
         # paste the relative energies into the properly formatted anpass file
-        # TODO replace this stuff with a classwide dict
-        # TODO clean up this garbage
-        anpass_temp = self.freqs_dir + "/anpass.tmp"
+        
+        #this is all to do with setting up anpass1.in
+        anpass_temp = self.freqs_files['anpass']
+        anpass1_in = self.input_files['anpass1']
+        anpass1_out = self.output_files['anpass1']
+        anpass2_in = self.input_files['anpass2']
+        anpass2_out = self.output_files['anpass2']
 
         with open(anpass_temp) as anpass_temp:
             anpass_lines = [line.strip("\n") for line in anpass_temp.readlines()]
 
-        datapoint_line_index = [
-            i for i, line in enumerate(anpass_lines) if "DATA POINTS" in line
-        ].pop()
+        datapoint_line_index = self.find_index(anpass_lines, 'DATA POINTS')
 
-        # TODO maybe +3
         anpass_header = anpass_lines[0 : datapoint_line_index + 3]
 
         number_of_points = int(anpass_header[-2].split()[0])
@@ -108,8 +130,6 @@ class QffHelper:
         for i in range(len(anpass_coords)):
             anpass_coords[i].append(self.relative_energies[i])
 
-        anpass_in = self.freqs_dir + "/anpass1.in"
-
         header_string = "\n".join(anpass_header)
         footer_string = "\n".join(anpass_footer)
 
@@ -118,27 +138,23 @@ class QffHelper:
             [coord_string_template % (tuple(line)) for line in anpass_coords]
         )
 
-        with open(anpass_in, "w") as f:
+
+        #this is where we actually write anpass1
+        with open(anpass1_in, "w") as f:
             f.write(header_string)
             f.write("\n")
             f.write(coord_string)
             f.write("\n")
             f.write(footer_string)
 
-        # actually runs anpass (first time)
-        anpass_out = self.freqs_dir + "/anpass1.out"
-
-        with open(anpass_in) as anpass_in, open(anpass_out, "w") as out_file:
-            subprocess.run(self.anpass_location, stdin=anpass_in, stdout=out_file)
+        #this is where we actually run anpass
+        self.run_program('anpass1')
 
         # now do anpass2
-        with open(anpass_out) as f:
+        with open(anpass1_out) as f:
             anpass1_lines = f.readlines()
 
-        #        energy_index = [
-        #            i for i, line in enumerate(anpass1_lines) if "WHERE ENERGY IS" in line
-        #        ].pop()
-
+        #This is all unique to anpass2
         energy_index = self.find_index(anpass1_lines, "WHERE ENERGY IS")
 
         self.anpass_refit_energy = float(
@@ -161,9 +177,6 @@ class QffHelper:
         # reform footer string
         footer_string = "\n".join(anpass_footer)
 
-        # write anpass2.in
-        anpass2_in = self.freqs_dir + "/anpass2.in"
-
         with open(anpass2_in, "w") as f:
             f.write(header_string)
             f.write("\n")
@@ -172,10 +185,7 @@ class QffHelper:
             f.write(footer_string)
 
         # now run anpass2.out
-        anpass2_out = self.freqs_dir + "/anpass2.out"
-        self.anpass2_out = anpass2_out
-        with open(anpass2_in) as anpass_in, open(anpass2_out, "w") as out_file:
-            subprocess.run(self.anpass_location, stdin=anpass_in, stdout=out_file)
+        self.run_program('anpass2')
         # TODO split run anpass and read anpass into separate functions? yes
 
     def find_index(self, _list_of_lines, _search_string):
@@ -198,19 +208,25 @@ class QffHelper:
                 j = i + 1
                 f.write("\n" + f"{j:5}      {disp:15}")
             f.write("\n    0")
-        with open(intgeom_file) as input_file, open(anpass2_out, "w") as out_file:
-            subprocess.run(self.anpass_location, stdin=anpass_in, stdout=out_file)
 
-    def run_program(self, _program, _basename):
-        """Runs a program"""
-        input_file = self.freqs_dir + "/" + _basename + ".in"
-        output_file = self.freqs_dir + "/" + _basename + ".out"
+        #with open(intgeom_file) as input_file, open(anpass2_out, "w") as out_file:
+        #    subprocess.run(self.anpass_location, stdin=anpass_in, stdout=out_file)
+        self.run_program('intder_geom')
 
-        # TODO change this to dict lookup
-        program = _program_location
+
+    def run_program(self, _program : str):
+        """Runs a program. Accepts anpass1 or anpass2 :)"""
+        input_file = self.input_files[_program]
+        output_file = self.output_files[_program]
+
+        if _program == 'anpass1' or _program == 'anpass2':
+            _program = 'anpass'
+        elif _program == 'intder_geom':
+            _program = 'intder'
+        executable = self.programs[_program]
 
         with open(input_file) as input_file, open(output_file, "w") as output_file:
-            subprocess.run(program, stdin=input_file, stdout=output_file)
+            subprocess.run(executable, stdin=input_file, stdout=output_file)
 
     def run_intder():
         """Reads intder_geom.out and runs intder"""
--- qff_helper/test_script.py
+++ qff_helper/test_script.py
@@ -1,10 +1,10 @@
 import qff_helper
 
-helper = qff_helper.QffHelper()
+helper = qff_helper.QffHelper('onepiecsde')
 
-helper.setup_spectro_dir('example_files/hcf/files/freqs/',
-        'example_files/hcf/pts/intder.in')
+helper.setup_spectro_dir('example_files/hcf/files/freqs/', 'example_files/hcf/pts/intder.in')
 
 helper.make_relative_energies('example_files/energy.dat')
 
 helper.run_anpass()
+helper.run_intder_geom()
