948a397789d3323fa61fad869365835c8fa2bbaa
Megan Christina Davis <r2610@maple.cm.cluster>
2022-09-01 11:43:09 -0500

    matdisp.pl works

--- qff_helper/cli.py
+++ qff_helper/cli.py
@@ -1,21 +1,26 @@
 """Console script for qff_helper."""
 import sys
 import click
-import qff_helper
 import qff_helper.qff_helper as qffh
 
+
 @click.command()
-@click.argument("files_dir") 
+@click.argument("files_dir")
 @click.argument("intder_geom_file")
 @click.argument("energy_dat")
 @click.argument("molecule")
 @click.argument("theory")
-@click.argument("freqs_dir")
-
-def main(files_dir, intder_geom_file, energy_dat, molecule, theory, freqs_dir="freqs"):
+@click.argument("freqs_dir", default="freqs")
+@click.option(
+    "--matdisp",
+    is_flag=True,
+    default=False,
+    help="uses matdisp for anpass mostly for HNCO",
+)
+def main(files_dir, intder_geom_file, energy_dat, molecule, theory, freqs_dir, matdisp):
     """Console script for qff_helper."""
     helper = qffh.QffHelper(freqs_dir)
-    helper.auto_spec(files_dir, intder_geom_file, energy_dat)
+    helper.auto_spec(files_dir, intder_geom_file, energy_dat, matdisp)
     helper.run_spec_to_latex(theory, molecule)
     helper.collect_misc_data()
     helper.format_force_constants()
